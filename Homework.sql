06efac2a0e1520b9321d0b46e47e9d61428c76fb
Matt Sanchez <msanchez@tql.com>
2021-05-16 14:11:33 -0400

    UpdatedSql

---
+++ Homework.sql
@@ -0,0 +1,96 @@
+/*use Master;
+create database DBHMWK;
+*/
+
+Use DBHMWK;
+create table Users( 
+
+Id int Primary Key Identity (1,1),
+Username varchar(30) not null unique,
+Password varchar(30) not null,
+FirstName varchar(30) not null,
+Lastname varchar(30) not null,
+Phone varchar(12),
+Email varchar(255),
+IsReviewer bit not null,
+IsAdmin bit not null
+);
+
+
+
+Create Table Vendors(
+
+Id Int Primary Key Identity (1,1),
+Code varchar(30) not null Unique,
+Name varchar(30) not null,
+Address varchar(30) not null,
+City varchar(30) not null,
+State char(2) not null,
+Zip varchar(5) not null,
+Phone varchar(12),
+email varchar(255)
+
+);
+
+CREATE table Products(
+Id int primary key identity (1,1),
+PartNbr varchar(30) not null Unique,
+Name varchar(30) not null ,
+Price decimal(11,2) not null,
+Unit varchar(30) not null,
+PhotoPath varchar(255),
+VendorId int Foreign Key References Vendors(Id)); 
+
+
+Create Table Requests( 
+Id Int Primary Key Identity (1,1),
+Description varchar (80) not null,
+Justification varchar(80) not null,
+RejectionReason varchar(80),
+DeliveryMode varchar(20) not null default 'Pickup',
+Status varchar(10) not null Default 'NEW',
+Total decimal(11,2) not null Default 0,
+UserId int not null Foreign Key References Users(Id));
+
+
+
+Create Table RequestLine(
+Id Int Primary Key Identity (1,1),
+RequestId Int not null Foreign Key References Requests(Id),
+ProductId Int not null Foreign Key References Products(Id),
+Quantity Int not null Default 1);
+
+
+
+Insert into Users(Username, Password, FirstName, Lastname, Phone, Email, IsReviewer, IsAdmin)
+	Values ('Msanchez', 'Password1', 'Matt', 'Sanchez', '813-503-8003', 'Msanchez@TQL.com', 1, 1),
+			( 'MasterSmith', 'Bobby1', 'Bob', 'Smith', '555-346-5577', 'BSmithy@Facebook.com', 0,0),
+			('DLimbacher', 'Dman1', 'David', 'Limbacher', '800-580-3101', 'Dlimbacher@tql.com', 1,0);
+
+
+Insert into Vendors(Code, Name, Address, City, State, Zip, Phone, email)
+	Values( 'AMZN', 'Amazon', '1 Amazon Way', 'Seattle', 'WA', '55555', '123-555-5567', 'Jbezos@amazon.com'),
+		  ( 'WMT', 'Walmart', '500 Main St', 'Little Rock', 'AR', '33557', '800-555-0127', 'SamWalton@Walmart.com'),
+		  ( 'MSFT', 'Microsoft', '1 Dos Ave', 'Seattle', 'WA', '44556', '555-555-1234', 'BillyGates@Microsoft.com');
+
+Insert into Products( PartNbr, Name, Price, Unit, VendorId)
+	values( 'Echo', 'Amazon Echo', 100, 1, (Select id from Vendors where code = 'AMZN')),
+		  ('Dot', 'Amazon Dot', 50, 1,  (Select id from Vendors where code = 'AMZN')),
+		  ('Home', 'Amazon Home', 150, 1, (Select id from Vendors where code = 'AMZN')),
+		  ('CAT', 'Cat Food', 5, 1,  (Select id from Vendors where code = 'WMT')),
+		  ('Oats', 'Quaker Oats', 2, 1, (Select id from Vendors where code = 'WMT')),
+		  ('TP', 'Charmin TP', 10, 1, (Select Id from Vendors where code = 'WMT')),
+		  ('MSFS', 'Microsoft Flight Sim', 200, 1, (Select id from Vendors where code = 'MSFT')),
+		  ('GAME', 'Minecraft', 50, 1,  (Select id from Vendors where code = 'MSFT')),
+		  ('HACK', 'Microsoft Hackers Net', 500, 1, (Select id from Vendors where code ='MSFT'));
+
+
+			
+		  select v.Name, Address, City, State, Price, Unit, (Price * Unit) 'Sale Price' from Vendors v
+		  join Products p on v.Id=p.VendorId		
+		  
+
+
+);
+
+
--- Tables-for-Capstone.sql
+++ Tables-for-Capstone.sql
@@ -19,6 +19,8 @@ IsReviewer bit not null,
 IsAdmin bit not null,
 );
 
+Select * from Users
+
 create table Vendors(
 
 Id int primary key identity (1,1) not null,
@@ -32,6 +34,7 @@ Phone varchar(12),
 Email varchar(255)
 );
 
+select * from Vendors
 Create table Products(
 
 Id int primary key identity (1,1) not null,
@@ -58,22 +61,35 @@ RequestId int not null Foreign Key References Requests(id),
 ProductId int not null Foreign Key References Products(id),
 Quantity Int not null Default 1,);
 
-/*
+
 Insert into Users(Username, Password, Firstname, Lastname, Phone, Email, IsReviewer, IsAdmin)
 	Values ('Msanchez', 'qwerty', 'Matt', 'Sanchez', '813-503-8003', 'msanchez@tql.com', 1, 1)
+		Values   ('GDoud', 'Password1', 'Greg', 'Doud', '513-555-5520', 'GDoud@TQL.com',0,1),
+		   ('BobSmith', 'H7d$235js', 'Bob', 'Smith', '813-555-2257', 'BSmith@gmail.com', 0,0);
 
 	Update Users set 
 	Phone = '813-503-8003'
 	where id = 1
 
-	select * from Users
-	*/
+	--select * from Users
+	
 
 	Insert into Vendors (Code, Name, Address, City, State, Zip, Phone, Email)
-		Values ('CBB', 'Colorado Box Beef', '123 Main St','Denver', 'CO', '12345', '247-555-1234', 'CBB@Beef.com') 
+		--Values ('CBB', 'Colorado Box Beef', '123 Main St','Denver', 'CO', '12345', '247-555-1234', 'CBB@Beef.com') 
+		Values('SAMS', 'Sams Club', '500 Walton Ave', 'Little Rock', 'AR', '33556', '347-455-5568', 'SWalton@Walmart.com'),
+				('AMZN', 'Amazon', '1 Amazon Way', 'Seattle' , 'WA', '12257', '777-777-7777', 'JBezos@Amazon.com')
+
 
 		select * from Vendors
+		select * from Products
 
-	Insert into Products(PartNbr, Name, Price, Unit, PhotoPath, VendorId)
-		Values('MOO', 'Waygu Beef', '1000', '1', 'NULL', 1)
+	Insert Products(PartNbr, Name, Price, unit, VendorId)
+		--Values('MOO', 'Waygu Beef', '1000' )
+		Values('MOO1', 'Beef Ribs', 250, 1, (Select id from Vendors where Code = 'CBB')),
+		('MOO2', 'Ground Beef', 20, 1, (Select id from Vendors where code = 'CBB'))
+
+
+		UPDATE Products set
+			Unit = 1
+			where id = 1
 
